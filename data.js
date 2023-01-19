e93bdec7b3501c8a30e15b44e118c551f5f83ca3
qrbaxter <qrbaxter90@gmail.com>
2023-01-19 14:10:00 +1100

    commti1
    sdfsdf

---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: `@LeoDaVinci`,
+        profilePic: `images/leovinci.jpg`,
+        likes: 1,
+        retweets: 0,
+        tweetText: `This is a sign, to continue living.`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@EelonMask`,
+        profilePic: `images/musk.png`,
+        likes: 283,
+        retweets: 1230,
+        tweetText: `Watcha all doin today frendos?`,
+        replies: [
+                  {
+                handle: `@monikerberruci`,
+                profilePic: `images/monicabelluci.png`,
+                tweetText: `Playin pool bro`,
+            },
+                  {
+                handle: `@JerzelBundkin`,
+                profilePic: `images/gisele.png`,
+                tweetText: `Brogramming`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@blazegerijabeth`,
+        profilePic: `images/grace.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `I like coding`,
+        replies: [
+            {
+                handle: `@nimonsessman`,
+                profilePic: `images/simon.png`,
+                tweetText: `Omg me too we have so much in common`,
+            },
+            {
+                handle: `@bonykahfud`,
+                profilePic: `images/toni.png`,
+                tweetText: `You are a QUEEN`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
